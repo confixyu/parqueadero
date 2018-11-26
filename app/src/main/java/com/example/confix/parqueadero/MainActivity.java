ee335bbf0b4685f441e4b6cfd4ed360e4471c465
Tony Yu <tyu@nativapps.co>
2018-11-26 14:50:29 -0500

    guardar

--- app/src/main/java/com/example/confix/parqueadero/MainActivity.java
+++ app/src/main/java/com/example/confix/parqueadero/MainActivity.java
@@ -6,10 +6,16 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
         txt_license = findViewById(R.id.txt_license);
         btn_add = findViewById(R.id.btn_add);
         //btn_delete = findViewById(R.id.btn_delete);
+
+        list_vehicle = findViewById(R.id.list_vehicle);
+
+        readAll();
 
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,17 +58,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void readAll(){
+        List<String> arraylist = new ArrayList<String>();
+
         BaseDatos baseDatosIntc = new BaseDatos(this, "Polo", null, 1);
         SQLiteDatabase BD = baseDatosIntc.getReadableDatabase();
 
-        Cursor row = BD.rawQuery("SELECT * FROM table_vehicle", null);
+        Cursor row = BD.rawQuery("SELECT * FROM VEHICLES", null);
+
+        while(row.moveToNext()) {
+            arraylist.add(row.getString(0));
+        }
+        txt_search.setText(arraylist.toString());
+
+        row.close();
+
+        BD.close();
     }
+
 
     public void getById(String searchId){
         BaseDatos baseDatosIntc = new BaseDatos(this, "Polo", null, 1);
         SQLiteDatabase BD = baseDatosIntc.getReadableDatabase();
 
-        Cursor row = BD.rawQuery("SELECT * FROM table_vehicle WHERE id=" + searchId, null);
+        Cursor row = BD.rawQuery("SELECT * FROM VEHICLES WHERE id=" + searchId, null);
 
         if(row.moveToFirst()){
             // AQUI SE AGREGA LA LISTA A LA LIST VIEW
@@ -77,9 +99,11 @@ public class MainActivity extends AppCompatActivity {
         v1.put("license", license);
         v1.put("time_in", time_in);
 
-        Long valor = BD.insert( "table_vehicle",null, v1);
+        Long valor = BD.insert( "VEHICLES",null, v1);
         Toast.makeText(this, "Guardado!"+valor, Toast.LENGTH_SHORT).show();
 
         txt_license.setText("");
+
+        BD.close();
     }
 }
