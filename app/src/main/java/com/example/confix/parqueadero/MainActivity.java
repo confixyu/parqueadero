9040ee8bf5857355d1b9d6c15ed062d371ff7c2a
Tony Yu <tonyyucen@gmail.com>
2018-12-01 14:05:09 -0500

    arreglar recyclerview

--- app/src/main/java/com/example/confix/parqueadero/MainActivity.java
+++ app/src/main/java/com/example/confix/parqueadero/MainActivity.java
@@ -3,19 +3,16 @@ package com.example.confix.parqueadero;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
         txt_license = findViewById(R.id.txt_license);
         btn_add = findViewById(R.id.btn_add);
-        //btn_delete = findViewById(R.id.btn_delete);
 
         list_vehicle = findViewById(R.id.list_vehicle);
 
@@ -46,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
             }
         });
 
@@ -89,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void getById(String searchId){
+    /*public void getById(String searchId){
         BaseDatos baseDatosIntc = new BaseDatos(this);
         SQLiteDatabase BD = baseDatosIntc.getReadableDatabase();
 
@@ -102,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         BD.close();
-    }
+    }*/
 
     public void save(String license, String time_in){
         BaseDatos baseDatosIntec = new BaseDatos(  this);
--- app/src/main/java/com/example/confix/parqueadero/VehicleAdapter.java
+++ app/src/main/java/com/example/confix/parqueadero/VehicleAdapter.java
@@ -1,14 +1,13 @@
 package com.example.confix.parqueadero;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
+import android.widget.ArrayAdapter;
 
-public class VehicleAdapter extends RecyclerView<VehicleAdapter.ViewHolder> {
+import java.util.ArrayList;
 
-    public VehicleAdapter(@NonNull Context context) {
-        super(context);
+//public class VehicleAdapter extends RecyclerView<VehicleAdapter.ViewHolder> {
+public class VehicleAdapter extends ArrayAdapter<Vehicle> {
+    public VehicleAdapter(Context context, ArrayList<Vehicle> Vehicles) {
+        super(context, 0, Vehicles);
     }
-
-
 }
